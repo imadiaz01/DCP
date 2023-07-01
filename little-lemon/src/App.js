8df3235282f504aa4bf21b9e48cfecd4ae2bfc8a
Imanol Diaz <imadiaz01@gmail.com>
2023-07-01 16:36:55 -0400

    fresh start react

---
+++ little-lemon/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    "Hello world! this is my capstone project from start!
+     Zack Diaz"
+
+    </div>
+  );
+}
+
+export default App;
